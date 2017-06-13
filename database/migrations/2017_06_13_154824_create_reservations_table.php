a5cdfac3c52815b79ed38b01b2d0793088905e5e
maltaesousa <sos@ylb.ch>
2017-06-13 18:13:48 +0200

    Added migrations with controllers and models

--- database/migrations/2017_06_13_154824_create_reservations_table.php
+++ database/migrations/2017_06_13_154824_create_reservations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateReservationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('login');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('role');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->dateTimeTz('begin');
+            $table->dateTimeTz('end');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reservations');
     }
 }
