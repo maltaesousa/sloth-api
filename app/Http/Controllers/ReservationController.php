e6cc909061a33999c1613a956289959796df6a12
maltaesousa <sos@ylb.ch>
2017-06-20 20:41:56 +0200

    Controller for reservations + JSON output

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Reservation;
 
 class ReservationController extends Controller
 {
-    //
+    public function index()
+    {
+        return Reservation::All();
+    }
 }
