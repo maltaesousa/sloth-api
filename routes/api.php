8106c0bd2910630579cd4ece822260ba3792d06e
maltaesousa <sos@ylb.ch>
2017-06-25 20:54:46 +0200

    tidy up routes

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
      return $request->user();
 });
 */
-/*
-Route::post('login', 'LoginController@login');
-Route::post('login', 'LoginController@refresh');
-
-Route::post('logout', 'LoginController@logout');
-*/
 
 Route::group(['namespace' => 'Api'], function () {
     Route::post('/login', 'UserController@login');
