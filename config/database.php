796ae35f6ef34c18a2249553ab722eee2538d7b3
maltaesousa <sos@ylb.ch>
2017-07-02 15:35:27 +0200

    database config

--- config/database.php
+++ config/database.php
@@ -82,8 +82,8 @@ return [
             'driver' => 'pgsql',
             'host' => parse_url(getenv("DATABASE_URL"))["host"],
             'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["postgres"],
-            'password' => parse_url(getenv("DATABASE_URL"))["postgres"],
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public'
